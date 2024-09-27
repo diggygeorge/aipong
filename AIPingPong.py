2d68ae02613aaac210414a95d383a575f23c041e
Daniel George <157053826+diggygeorge@users.noreply.github.com>
2024-09-26 20:39:07 -0400

    changed caption title

--- AIPingPong.py
+++ AIPingPong.py
@@ -16,7 +16,7 @@ SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-pygame.display.set_caption('Kamala 2024')
+pygame.display.set_caption('cool ball thingy')
 
 #game variables
 WHITE = (255, 255, 255)
